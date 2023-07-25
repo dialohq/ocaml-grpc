9de1d336a6e4046f1528cb5eeb9f26500272d169
Daniel Quernheim <quernd@users.noreply.github.com>
2023-07-25 12:02:44 +0200

    Fix `read_once` and expose as `schedule_read`

--- lib/grpc-eio/stream.mli
+++ lib/grpc-eio/stream.mli
@@ -1,6 +1,10 @@
 type t
 
 val of_h2_body : H2.Body.Reader.t -> t
+
+val schedule_read :
+  on_msg:(string -> unit) -> on_eof:(unit -> unit) -> t -> unit
+
 val read_loop : on_msg:(string -> unit) -> on_eof:(unit -> unit) -> t -> unit
 val take : t -> string option Eio.Promise.t
 val to_seq : t -> string Seq.t
